9792428e9b48e29abb7b1a8b508fb95a2c4d521f
codemanic001 <workingnitesh1@gmail.com>
2026-05-28 08:28:32 +0530

    This is the updated code

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(word,times) {
+if(times<0) return "ERROR";
+let String=""
+for(i=0;i<times;i++)
+{
+    String +=word;
+}
+return(String)
 };
 
 // Do not edit below this line
