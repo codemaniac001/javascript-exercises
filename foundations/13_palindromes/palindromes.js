85fb7aa9de7a816a2664162f7786f1ef73680b54
codemanic001 <workingnitesh1@gmail.com>
2026-05-30 09:17:21 +0530

    This is the palindrome updated

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+   const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789'
+   const cleanedstring=word
+   .toLowerCase()
+   .split('')
+   .filter((character) => alphanumerical.includes(character))
+   .join('');
+   const reversedstring=cleanedstring.split('').reverse().join('');
+   return(reversedstring === cleanedstring);
 };
 
-// Do not edit below this line
 module.exports = palindromes;
+
