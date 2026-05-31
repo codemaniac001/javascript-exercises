ff10f14103c2e3cf3a735d25b507298ac0ecacd7
codemanic001 <workingnitesh1@gmail.com>
2026-05-31 08:43:36 +0530

    This is the fibonacci,updated

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+   let a=0;b=1;
+   let sum1=0;
+   n1=Number(n)
+   if(n<0||(n1===NaN))
+   {
+      return('OOPS')
+   }
 
-};
+   if(n==0)
+   {
+      return(0)
+   }
+   else if(n==1)
+   {
+      return(1)
+   }
+   else
+   {
+      for(i=1;i<n;++i)
+      {
+         sum1=a+b;
+         a=b;
+         b=sum1;
+      }
+   }
+   return(sum1)
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
